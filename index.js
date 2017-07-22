66512623dd9bebd2c642a1ccfc2c171a83860333
Chris <orthagonal1@gmail.com>
2017-07-22 17:02:09 -0500

    version 1

---
+++ index.js
@@ -0,0 +1,24 @@
+const async = require('async');
+
+module.exports = (collection, schema, method, callback) => {
+  // cache of id's that were previously seen:
+  const key = schema.key;
+  const property = schema.set;
+  const cache = {};
+  async.each(collection, (collectionItem, eachDone) => {
+    // don't reprocess if it's cached:
+    if (cache[collectionItem[key]]) {
+      collectionItem[key][property] = cache[collectionItem[key]];
+    }
+    method(collectionItem[key], (err, fetchedItem) => {
+      if (err) {
+        return callback(err);
+      }
+      cache[collectionItem[key]] = fetchedItem;
+      collectionItem[property] = fetchedItem;
+    });
+    eachDone();
+  }, (err) => {
+    callback(err, collection);
+  });
+};
---
+++ test/test.objoin.js
@@ -0,0 +1,36 @@
+'use strict';
+const test = require('tap').test;
+const objoin = require('../index.js');
+
+test('logger defaults to console.log', (t) => {
+  const posts = [
+    { authorId: 'id1', title: 'this is post 1' },
+    { authorId: 'id2', title: 'this is post 2' },
+    { authorId: 'id1', title: 'this is post 3' }
+  ];
+  const users = {
+    id1: { name: 'bob smith' },
+    id2: { name: 'jane brown' }
+  };
+  objoin(posts, { key: 'authorId', set: 'author' }, (authorId, next) => {
+    //authorIds are just unique Ids, so you don't have to fetch the same id multiple times
+    //in this case, it would get called with authorId id1 and id2 (the second id1 would not be called)
+    //normally this would be some call to the db or ajax call
+    next(null, users[authorId]);
+  }, (err, obj) => {
+    t.equal(err, null);
+    t.equal(obj[0].authorId, 'id1');
+    t.equal(obj[1].authorId, 'id2');
+    t.equal(obj[2].authorId, 'id1');
+
+    t.equal(obj[0].title, 'this is post 1');
+    t.equal(obj[1].title, 'this is post 2');
+    t.equal(obj[2].title, 'this is post 3');
+
+    t.equal(obj[0].author.name, 'bob smith');
+    t.equal(obj[1].author.name, 'jane brown');
+    t.equal(obj[2].author.name, 'bob smith');
+
+    t.end();
+  });
+});
