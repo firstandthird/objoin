8b8de7b045ef3109f145318e1303f798f6cc0c1a
Chris <orthagonal1@gmail.com>
2017-12-23 20:43:13 -0600

    basic stuff

--- index.js
+++ index.js
@@ -1,86 +1,63 @@
 const async = require('async');
 
-module.exports = (collection, schema, method, callback) => {
+module.exports = async(collection, schema, method) => {
   const key = schema.key;
   const property = schema.set;
   const get = schema.get;
-  const cache = {};
 
-  // assigns either a specific field or the whole object:
-  // for either an array or a field:
-  const assign = (destination, source) => {
-    const element = get ? source[get] : source;
-    if (Array.isArray(destination)) {
-      destination.push(element);
-    } else {
-      destination[property] = element;
-    }
-  };
-
-  // applies the fetch method one time to one item:
-  const applySingleMethod = (collectionItem, done) => {
-    if (cache[collectionItem[key]]) {
-      assign(collectionItem, cache[collectionItem[key]]);
-      return done();
-    }
-    return method(collectionItem[key], (err, fetchedItem) => {
-      if (err) {
-        return done(err);
-      }
-      cache[collectionItem[key]] = fetchedItem;
-      assign(collectionItem, fetchedItem);
-      done();
-    });
-  };
-
-  // applies the fetch method one time each to an array of items:
-  const applyMultipleMethod = (collectionItem, done) => {
-    const allFetchedItems = [];
-    async.eachSeries(collectionItem[key], (listItem, eachDone) => {
-      if (cache[listItem]) {
-        assign(allFetchedItems, cache[listItem]);
-        return eachDone();
-      }
-      method(listItem, (err, fetchedItem) => {
-        if (err) {
-          return done(err);
-        }
-        cache[listItem] = fetchedItem;
-        assign(allFetchedItems, fetchedItem);
-        eachDone();
-      });
-    }, (err) => {
-      if (err) {
-        return done(err);
-      }
-      collectionItem[property] = allFetchedItems;
-      return done();
-    });
-  };
-
-  const handleCallback = (err) => {
-    if (err) {
-      return callback(err);
-    }
-    return callback(null, collection);
-  };
-
-  // if collection was just one item, just process and return it by itself:
+  /// make sure collection is an array:
   if (!Array.isArray(collection)) {
-    if (!Array.isArray(collection[key])) {
-      return applySingleMethod(collection, handleCallback);
+    collection = [collection];
+  }
+  // get unique list of keys in the collection:
+  // const uniqueKeys = Array.from(new Set(collection.map(item => item[key])));
+  const set = new Set();
+  collection.forEach((item) => {
+    if (Array.isArray(item[key])) {
+      item[key].forEach((curKey) => {
+        set.add(curKey);
+      });
+    } else {
+      set.add(item[key]);
     }
-    // the collection's field is an array of items to fetch here:
-    return applyMultipleMethod(collection, handleCallback);
+  });
+  const uniqueKeys = Array.from(set);
+
+  const promiseList = [];
+  // get unique item list without blocking::
+  uniqueKeys.forEach((itemKey) => {
+    promiseList.push(method(itemKey));
+  });
+  const uniqueItemList = await Promise.all(promiseList);
+
+  // organize them by key:
+  const uniqueItemDict = {};
+  for (let i = 0; i < uniqueKeys.length; i++) {
+    uniqueItemDict[uniqueKeys[i]] = uniqueItemList[i];
   }
 
-  // otherwise, if collection is multiple items:
-  return async.each(collection, (collectionItem, eachDone) => {
-    // if each field of the collection item is one item:
-    if (!Array.isArray(collectionItem[key])) {
-      return applySingleMethod(collectionItem, eachDone);
+  // set the requested property field:
+  collection.forEach((item) => {
+    console.log('item')
+    console.log('item')
+    console.log('item')
+    console.log(item)
+    const curKey = item[key];
+    let getProperty = () => get ? uniqueItemDict[curKey][get] : uniqueItemDict[curKey];
+    let setProperty = () => {
+      item[property] = getProperty();
+    };
+    if (Array.isArray(curKey)) {
+      setProperty = () => {
+        const allValues = [];
+        curKey.forEach((individualKey) => {
+          allValues.push(get ? uniqueItemDict[individualKey][get] : uniqueItemDict[individualKey]);
+        });
+        item[property] = allValues;
+      };
     }
-    // if the collection item is a list of multiple items:
-    return applyMultipleMethod(collectionItem, eachDone);
-  }, handleCallback);
+    setProperty();
+  });
+
+  return collection;
 };
